6db5afd56271024185b177f0777a1823ce51a1da
Tyomichh <workemailaertemlozenko159@gmail.com>
2023-04-18 13:46:33 +0300

    Added adaptive burger

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,52 @@
-/*Smooth scroll*/
-$("[data-scroll]").on("click", function (event) {
-    event.preventDefault();
+$(function () {
+    var header = $(".header"),
+        introH = $("#NFTs").innerHeight(),
+        scrollOffset = $(window).scrollTop();
 
-    var blockID = $(this).data('scroll'),
-        blockOffset = $(blockID).offset().top;
 
-    $("#nav a").removeClass("active");
-    $(this).addClass("active");
+    /*FIXED HEADER*/
+    checkSkroll(scrollOffset);
 
-    $("html, body").animate({
-        scrollTop: blockOffset
-    }, 500);
+    $(window).on("scroll", function () {
+        scrollOffset = $(this).scrollTop();
+
+        checkSkroll(scrollOffset);
+
+    });
+
+    function checkSkroll(scrollOffset) {
+
+        if (scrollOffset >= introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+    }
+
+
+    /*Smooth scroll*/
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        var blockID = $(this).data('scroll'),
+            blockOffset = $(blockID).offset().top;
+
+        $("#nav a").removeClass("active");
+        $(this).addClass("active");
+
+        $("html, body").animate({
+            scrollTop: blockOffset
+        }, 500);
+    });
+    
+
+    /*Mneu nav toggle*/
+    $("#nav_toggle").on("click", function (event) {
+        event.preventDefault();
+
+        $(this).toggleClass("active");
+        $("#nav").toggleClass("active");
+
+    });
 });
